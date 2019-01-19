81e099b0fc09b2149bd8b1057331057a2fdfc586
qetyzm <qetyzm@gmail.com>
2019-01-19 23:03:03 +0100

    Added configs

--- example/smakeconf.py
+++ example/smakeconf.py
@@ -1,27 +1,19 @@
 import smake
-import itertools
 
 if __name__ == "__main__":
     sm = smake.Smake()
     sm.name = "app"
-    sm.lang = "C"
-    #sm.target = "build/" + sm.name
-    #sm.obj_dir = "obj"
     sm.gcc.sources = sm.merge(
         sm.wildcard("src/*.c"), 
         sm.wildcard("src/module_1/*.c"), 
         sm.wildcard("src/module_2/*.c"))
     sm.gcc.warning_flags = ["all", "extra"]
-    sm.gcc.compiler_options = ["pedantic"]
-    sm.load_package("SDL2")
+    sm.gcc.compiler_options = ["pedantic", "std=c11"]
 
-    # sm.load_package() has SDL2 in repository so it will automatically set all the flags
-    #sm.linker_flags = ["sdl2"]
-    #sm.include_flags = []
-    #sm.include_dirs = ["C:\\SDL2\\include"]
-    #sm.library_dirs = ["C:\\SDL2\\lib"]
+    sm.linker_flags = ["sdl2"]
+    sm.include_dirs = ["C:\\SDL2\\include"]
+    sm.library_dirs = ["C:\\SDL2\\lib"]
 
-    #if sm.gcc.available():
     sm.gcc.link()
     sm.gcc.compile()
 
