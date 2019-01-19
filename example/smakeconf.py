6b7c4bdd24a380bb0c48a6055b60617a0c1eeb8a
qetyzm <qetyzm@gmail.com>
2019-01-19 16:29:11 +0100

    Modified smackconf.py example

--- example/smakeconf.py
+++ example/smakeconf.py
@@ -2,16 +2,23 @@ import smake
 
 sm = Smake()
 sm.name = "app"
-sm.source_dirs = ["src", "src/module_1", "src/module_2"]
-sm.compiler = "gcc"
-sm.dependencies = ["sdl"]
-sm.target = "build/" + sm.name
+sm.lang = "C"
+sm.gcc.sources = ["src/*.c", "src/module_1/*.c", "src/module_2/*.c"]
+sm.gcc.compiler_options = "-Wall -pedantic"
+sm.gcc.target = "build/" + sm.get_platform()
+sm.gcc.obj_dir = "obj"
+sm.gcc.load_package("sdl2")
 
-def post_compile():
-    if sm.util.get_platform() == "linux":
+# sm.load_package() has SDL2 in repository so it will automatically set all the flags
+#sm.linker_flags = ["sdl2"]
+#sm.include_flags = []
+#sm.include_dirs = ["C:\\SDL2\\include"]
+#sm.library_dirs = ["C:\\SDL2\\lib"]
+
+if __name__ == "__main__":
+    sm.gcc.compile()
+
+    # post compile stuff
+    if sm.get_platform() == "linux":
         target = "/usr/bin/" + sm.name
         sm.copy_executable_to(target)
-
-
-sm.post_compile_script = post_compile
-sm.compile()
