1eebcab7c28e0932395228fe12f372554d35ca97
qetyzm <46635216+qetyzm@users.noreply.github.com>
2019-01-22 07:19:56 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,12 @@ setup(
     packages=['smake_buildtools'],
     entry_points={
     },
-    install_requires=[],
-    description='Simple Make replacement',
+    install_requires=[
+        'platform',
+        'glob',
+        'subprocess'
+    ],
+    description='Simple Make replacement for developers who love Python',
     author='Qetyzm',
     author_email='qetyzm@gmail.com',
     url='https://github.com/qetyzm/smake',
